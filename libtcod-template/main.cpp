5c28a062f8ee350c8eff1182d191e1a783222871
Chaz Straney <chaz@jackrussellsoftware.com>
2013-05-24 19:23:48 -0400

    Shiggity.

---
+++ libtcod-template/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "libtcod.hpp"
+
+int main() {
+    TCODConsole::initRoot(80, 50, "C++ libtcod tutorial", false);
+    TCODConsole::root->print(40,25,"Hello world");
+    TCODConsole::flush();
+    TCODConsole::waitForKeypress(true);
+    return 0;
+}
